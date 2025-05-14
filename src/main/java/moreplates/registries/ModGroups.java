75c6032c3f4dcbe41cf58d08aa8a4097d475b12d
001damege <chayama.koji0401@gmail.com>
2025-05-15 00:14:16 +0900

    テスト用でタブアイコンを変え忘れた

--- src/main/java/moreplates/registries/ModGroups.java
+++ src/main/java/moreplates/registries/ModGroups.java
@@ -9,7 +9,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.function.Consumer;
@@ -21,7 +20,7 @@ public class ModGroups {
     public static final ResourceKey<CreativeModeTab> MAIN = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MorePlates.MODID, "tab"));
 
     public static final RegistryEntry<CreativeModeTab> MAIN_TAB =
-            create(MAIN, "tab", "MorePlates", tab -> tab.icon(() -> new ItemStack(Items.ACACIA_BUTTON.asItem())).withTabsBefore(CreativeModeTabs.SPAWN_EGGS));
+            create(MAIN, "tab", "MorePlates", tab -> tab.icon(() -> new ItemStack(ModItems.STAR_PLATE.asItem())).withTabsBefore(CreativeModeTabs.SPAWN_EGGS));
 
     private static RegistryEntry<CreativeModeTab> create(ResourceKey<CreativeModeTab> key, String name, String english, Consumer<CreativeModeTab.Builder> consumer) {
         return REGISTRATE.generic(name, Registries.CREATIVE_MODE_TAB, () -> {
